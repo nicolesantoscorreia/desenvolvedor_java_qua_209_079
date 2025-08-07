02604b5444582070d7f05a1115a387176107a166
nicolesantoscorreia <nicsanolivy@gmail.com>
2025-08-07 17:44:54 -0300

    Aula do dia 07/08/2025

--- Atividades/Atividade01/src/com/atividade01/app/App.java
+++ Atividades/Atividade01/src/com/atividade01/app/App.java
@@ -4,63 +4,84 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /*!SECTION
-         * Criar um programa que receba o nome, peso e altura de usuário,
-         * e calcule o valor de seu IMC (Índice de Massa Corporal),
-         * cuja fórmula é peso/altura*altura, e informe o seu diagnóstico
-         * baseado na tabela do IMC (pesquisa na internet pela tabela),
-         * O programa poderá calcular o IMC de vários usuários,
-         * e deverá ter a opção de sair do programa.
-         */
+        // Instância de Scanner
         Scanner leia = new Scanner(System.in);
 
-        String nome;
+        // Declaração de variáveis
+        String nome, opcao, diagnostico; // opcao
         double peso, altura, imc;
-        int opcao;
+        int saida = 0;
 
+        // Loop
         do {
+            // Menu
             System.out.println("1 - Calcular IMC.");
             System.out.println("2 - Sair do programa.");
             System.out.println("Informe a opção desejada.");
-            opcao = leia.nextInt();
+            opcao = leia.nextLine();
 
-            if (opcao == 1) {
-                leia.nextLine();
-                System.out.println("Informe seu nome:");
-                nome = leia.nextLine();
-                System.out.println("Informe seu peso:");
-                peso = leia.nextDouble();
-                System.out.println("Informe sua altura:");
-                altura = leia.nextDouble();
-                
-                imc = peso / (altura*altura);
+            switch (opcao) {
+                case "1":
+                    System.out.println("Informe seu nome:");
+                    nome = leia.nextLine();
+                    System.out.println("Informe seu peso:");
+                    peso = leia.nextDouble();
+                    System.out.println("Informe sua altura:");
+                    altura = leia.nextDouble();
 
-                // System.out.println(imc);
+                    imc = peso / (altura*altura);
 
-                if (imc <= 18.5) {
-                    System.out.println(nome +  " está abaixo do peso.");
-                }
-                else if (imc > 18.5 && imc <= 24.9) {
-                    System.out.println(nome + " está com o peso normal.");
-                }
-                else if (imc >= 25 && imc <= 29.9) {
-                    System.out.println(nome + " está com sobrepeso.");
-                }
-                else if (imc >= 30 && imc <= 34.9) {
-                    System.out.println(nome + " está com obesidade grau I.");
-                }
-                else if (imc >= 35 && imc <= 39.9) {
-                    System.out.println(nome + " está com obesidade grau II.");
-                }
-                else {
-                    System.out.println(nome + " está com obesidade grau III.");
-                }
-            
-            } else if (opcao != 2) {
-                System.out.println("Opção inválida.");
-            }
-        } while (opcao != 2);
+                    System.out.println("O IMC do " + nome + " é " + String.format("%.2f", imc) + ".");
 
+                    diagnostico = (imc < 18.5) ? nome +  " está abaixo do peso." 
+                    : (imc < 25) ? nome + " está com o peso normal."
+                    : (imc < 30) ? nome + " está com sobrepeso."
+                    : (imc < 35) ? nome + " está com obesidade grau I."
+                    : (imc < 40) ? nome + " está com obesidade grau II."
+                    : nome + " está com obesidade grau III.";
+
+                    System.out.println(diagnostico);
+
+                    /* 
+                    if (imc < 18.5) {
+                        System.out.println(nome +  " está abaixo do peso.");
+                    }
+                    else if (imc > 18.5 && imc <= 24.9) {
+                        System.out.println(nome + " está com o peso normal.");
+                    }
+                    else if (imc >= 25 && imc <= 29.9) {
+                        System.out.println(nome + " está com sobrepeso.");
+                    }
+                    else if (imc >= 30 && imc <= 34.9) {
+                        System.out.println(nome + " está com obesidade grau I.");
+                    }
+                    else if (imc >= 35 && imc <= 39.9) {
+                        System.out.println(nome + " está com obesidade grau II.");
+                    }
+                    else {
+                        System.out.println(nome + " está com obesidade grau III.");
+                    }
+                    */
+                    break;
+                case "2":
+                    saida = Integer.parseInt(opcao);
+                    System.out.println("Programa encerrado.");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }    
+        } while (saida != 2);
+
+        // Fecha o objeto Scanner
         leia.close();
     }
 }
+/*!SECTION
+ * Criar um programa que receba o nome, peso e altura de usuário,
+ * e calcule o valor de seu IMC (Índice de Massa Corporal),
+ * cuja fórmula é peso/altura*altura, e informe o seu diagnóstico
+ * baseado na tabela do IMC (pesquisa na internet pela tabela),
+ * O programa poderá calcular o IMC de vários usuários,
+ * e deverá ter a opção de sair do programa.
+ */
+
--- Atividades/Atividade02/src/com/atividade02/app/App.java
+++ Atividades/Atividade02/src/com/atividade02/app/App.java
@@ -4,7 +4,67 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /*!SECTION
+        // Instância da classe Scanner
+        Scanner leia = new Scanner(System.in);
+
+        // Declaração de variáveis 
+        String nome, sala;
+        int idade, idadeMinima = 0;
+
+        // Input
+        System.out.println("Informe seu nome:");
+        nome = leia.nextLine();
+        System.out.println("Informe sua idade:");
+        idade = leia.nextInt();
+
+        // Limpeza de buffer
+        leia.nextLine();
+
+        do {
+            // Salas
+            System.out.println("☕ Cine Java ☕\n");
+            System.out.println("Sala 1 - Como treinar seu dragão (Livre)");
+            System.out.println("Sala 2 - Jumanji (12 anos)");
+            System.out.println("Sala 3 - Jurassic World (14 anos)");
+            System.out.println("Sala 4 - Matrix (16 anos)");
+            System.out.println("Sala 5 - Deadpool (18 anos)");
+            System.out.println("Qual sala deseja entrar:");
+            sala = leia.nextLine();
+
+            switch (sala) {
+                case "1":
+                    break;
+                case "2":
+                    idadeMinima = 12;
+                    break;
+                case "3":
+                    idadeMinima = 14;
+                    break;
+                case "4":
+                    idadeMinima = 16;
+                    break;
+                case "5":
+                    idadeMinima = 18;
+                    break;
+                default:
+                    idadeMinima = idade + 1;
+                    System.out.println("Sala inexistente.");
+            }
+
+            if (idade >= idadeMinima) {
+                System.out.println("Entrada permitida para " + nome + ".");
+                System.out.println("Tenha um bom filme! 🎟");
+            }
+            else {
+                System.out.println("Favor escolher outro filme!");
+            }
+        } while (idade < idadeMinima);
+
+        leia.close();
+    }
+}
+
+/*!SECTION
          * Crie uma aplicação em Java em que o usuário informe o nome 
          * e a idade, e o programa exibe 5 salas de cinema, cada uma
          * exibindo um filme com uma classificação indicativa.
@@ -13,11 +73,3 @@ public class App {
          * o ingresso e encerra. Caso usuário não tenha a idade mínima,
          * o programa proíbe a entrada e pede para escolher outro filme.
          */
-        Scanner leia = new Scanner(System.in);
-
-        String nome;
-        int idade, opcao;
-
-        leia.close();
-    }
-}
